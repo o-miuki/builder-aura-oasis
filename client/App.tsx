8e98beff53d4e21007381292c1da840c23166545
Miuki <42397998+o-miuki@users.noreply.github.com>
2025-07-21 14:23:30 -0300

    Fix missing widget-customizer route

--- client/App.tsx
+++ client/App.tsx
@@ -21,6 +21,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/settings" element={<Settings />} />
+          <Route path="/widget-customizer" element={<Settings />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
